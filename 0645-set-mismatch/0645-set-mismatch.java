ddbcdb81bfae2bec3b65ddaa51615bd290c6c5f4
Pramod Kumar Shah Sudi <pm3570955@gmail.com>
2026-07-23 02:15:15 +0530

    Time: 2 ms (94.55%), Space: 47.9 MB (24.89%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int duplicate = -1, missing = -1;
+
+        // Count frequency
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Find duplicate and missing
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                duplicate = i;
+            } else if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
