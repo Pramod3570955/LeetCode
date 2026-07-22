ca5e96c2645405400f4076e2deec04125a2a3a0c
Pramod Kumar Shah Sudi <pm3570955@gmail.com>
2026-07-23 01:17:14 +0530

    Time: 3 ms (86.79%), Space: 47.4 MB (93.14%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int candy(int[] ratings)
+    {
+        int n = ratings.length;
+        int []candies = new int[n];
+        Arrays.fill(candies,1);
+
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        for(int j=n-2;j>=0;j--)
+        {
+            if(ratings[j]>ratings[j+1])
+            {
+                candies[j] = Math.max(candies[j],candies[j+1]+1);
+            
+            }
+        }
+        int totalCandies = 0;
+        for(int candy:candies)
+        {
+            totalCandies +=candy;
+        }
+        return totalCandies;
+    }
+}
