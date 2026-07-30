2055d94fa2ce3dec03b50b627b53b2454f7e8a72
Pramod Kumar Shah Sudi <pm3570955@gmail.com>
2026-07-31 02:20:59 +0530

    Time: 0 ms (100%), Space: 43.2 MB (19.14%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+        for(int i = 0; i < n; i++){
+            currentAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+        return maxAltitude;
+    }
+}
