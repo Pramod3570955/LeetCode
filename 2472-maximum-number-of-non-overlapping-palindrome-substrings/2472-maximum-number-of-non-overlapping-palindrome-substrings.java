94224bce3b3c0492edced09feb55b9235f10f8cd
Pramod Kumar Shah Sudi <pm3570955@gmail.com>
2026-09-15 23:44:55 +0530

    Time: 115 ms (32.16%), Space: 62.3 MB (13.57%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+
+        int n = s.length();
+
+        // palindrome[i][j] = true if s[i...j] is a palindrome
+        boolean[][] palindrome = new boolean[n][n];
+
+        // Build palindrome table
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+
+                if (s.charAt(i) == s.charAt(j) &&
+                    (j - i <= 1 || palindrome[i + 1][j - 1])) {
+
+                    palindrome[i][j] = true;
+                }
+            }
+        }
+
+        // dp[i] = maximum number of valid palindromes
+        // using the first i characters
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            // Don't end a palindrome at i-1
+            dp[i] = dp[i - 1];
+
+            // Try every palindrome ending at i-1
+            for (int start = 0; start < i; start++) {
+
+                if (i - start >= k && palindrome[start][i - 1]) {
+
+                    dp[i] = Math.max(
+                        dp[i],
+                        dp[start] + 1
+                    );
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
